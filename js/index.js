592298aa7a63a2fe2cc79e535ff1e79da7db1036
ashleighliggett <liggetas@gmail.com>
2015-05-30 16:37:18 -0400

    Initial commit
    Uploading my design files to Github.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+	dots: false,
+  	centerMode: true,
+  	focusOnSelect: true
+  });
+   $('.hamburger').sidr();
+});
