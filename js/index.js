f8acff3ec3a582f600584221231350c25c4b622f
ashleighliggett <liggetas@gmail.com>
2015-06-01 00:33:09 -0400

    center carousel
    …

--- js/index.js
+++ js/index.js
@@ -18,12 +18,12 @@ $(document).ready(function(){
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 2
+        slidesToShow: 2,
+        slidesToScroll: 1
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 400,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
